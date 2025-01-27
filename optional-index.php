fcd66f32b412140b991bc7d6930746dcd51e05d1
Jessica <134755007+HelloJessicaM@users.noreply.github.com>
2025-01-26 23:22:31 -0500

    Create optional-index.php
    optional code for index.php of where your images are uploaded - this is the exact way I did it

---
+++ optional-index.php
@@ -0,0 +1,14 @@
+//optional code for index.php of where your images are uploaded - this is the exact way I did it//
+<?php
+echo "Here are our files <br />";
+$path = ".";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='$path/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
